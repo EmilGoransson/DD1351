8502d67fe4405c6eb7f642c6f4401176b6f1e9ac
EmilGoransson <emilgo@kth.se>
2023-08-07 14:58:57 +0200

    imp-ele now works
    fixed so that known list of proos"build up" as the program itterates though the complete proof (no spoilers)

--- labb2.pl
+++ labb2.pl
@@ -1,24 +1,29 @@
-Premiss=[imp(p, q), p].
+%Premiss=[imp(p, q), p].
 
-Goal='q'.
+%Goal='q'.
 
-Proof=[[1, imp(p,q), premise],[2, p,premise],[3, q,impel(2,1)]].
+%Proof=[[1, imp(p,q), premise],[2, p,premise],[3, q,impel(2,1)]].
 
 valid_proof(Premiss, Goal, Proof):-
-    valid_proof(Premiss, Goal, Proof, Proof).
+    valid_proof(Premiss, Goal, Proof, []).
 
 %stops when proof is at end
-valid_proof(_, _, []).
+valid_proof(_, _, [],_).
 
 
-valid_proof(Premiss, Goal, [[Row, CurProof, premise]|Rest], AllProof):-
+valid_proof(Premiss, Goal, [[Row, CurProof, premise]|Rest], ProofUntilNow):-
     member(CurProof, Premiss),!,
     writeln(CurProof),
-    valid_proof(Premiss, Goal, Rest, AllProof).
+    append(ProofUntilNow,[[Row, CurProof, premise]], NewProofUntilNow),
+    valid_proof(Premiss, Goal, Rest, NewProofUntilNow).
 %imp, impel, implication elimination
-valid_proof(Premiss, Goal, [[Row, CurProof, impel(Row1,Row2)]|Rest], AllProof):-
-    member([Row1, AltProof,_], AllProof),!,
-    member([Row2, imp(AltProof,CurProof),_], AllProof),!.
+valid_proof(Premiss, Goal, [[Row, CurProof, impel(Row1,Row2)]|Rest], ProofUntilNow):-
+    writeln(CurProof),
+    member([Row1, AltProof,_], ProofUntilNow),!,
+    member([Row2, imp(AltProof,CurProof),_], ProofUntilNow),!,
+    append(ProofUntilNow, [[Row, CurProof, premise]], NewProofUntilNow),
+    valid_proof(Premiss, Goal, Rest, NewProofUntilNow).
+
 
     
 
